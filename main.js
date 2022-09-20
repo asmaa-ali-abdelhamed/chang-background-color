fa27efa523762aa7b2cd510ca7eeb2cb2ee674bc
Asmaa Ali <asmaaali2495@gmail.com>
2022-09-20 21:06:55 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let img = document.querySelector('#img');
+let contaner = document.querySelector('.contaner');
+
+function mobiel(mob){
+    img.src = mob;
+}
+function colors(color){
+    contaner.style.background = color;
+}
